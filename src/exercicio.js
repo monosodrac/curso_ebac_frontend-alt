54f989ceccca3dde0eb237e98d0176bd8a600d90
monosodrac <monosodrac@gmail.com>
2025-01-08 23:34:37 -0300

    exercicio_ts

---
+++ src/exercicio.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicacao(multiplicador1, multiplicador2) {
+    return multiplicador1 * multiplicador2;
+}
+;
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+;
+console.log(saudacao('Mundo'));
---
+++ src/exercicio.ts
@@ -0,0 +1,9 @@
+function multiplicacao(multiplicador1: number, multiplicador2: number): number {
+    return multiplicador1 * multiplicador2;
+};
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+};
+
+console.log(saudacao('Mundo'));
